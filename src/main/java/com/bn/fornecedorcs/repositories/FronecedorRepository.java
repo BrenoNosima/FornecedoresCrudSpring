2c403b38a1c79271472d94ad7b2ac69ca31164c2
Nosima <breno.nosima@gmail.com>
2026-03-20 19:47:08 -0300

    repositor criado

---
+++ src/main/java/com/bn/fornecedorcs/repositories/FronecedorRepository.java
@@ -0,0 +1,7 @@
+package com.bn.fornecedorcs.repositories;
+
+import com.bn.fornecedorcs.models.FornecedorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FronecedorRepository extends JpaRepository<FornecedorModel, Long> {
+}
