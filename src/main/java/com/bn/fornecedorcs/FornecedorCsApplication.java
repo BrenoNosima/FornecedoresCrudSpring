3c8304e250b8d40c2355923c7cddfab4aaf17709
Nosima <breno.nosima@gmail.com>
2026-03-20 19:42:45 -0300

    frist commit

---
+++ src/main/java/com/bn/fornecedorcs/FornecedorCsApplication.java
@@ -0,0 +1,13 @@
+package com.bn.fornecedorcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornecedorCsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FornecedorCsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bn/fornecedorcs/FornecedorCsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.fornecedorcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornecedorCsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
